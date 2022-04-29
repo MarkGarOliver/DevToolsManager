dfacd36fc80a7c5b5fe358b11e2ae0bf5ae950fb
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2022-04-29 08:24:01 -0300

    finalizado passGenerator funcionando

--- routes/passGenerator.js
+++ routes/passGenerator.js
@@ -11,7 +11,32 @@ const res = require('express/lib/response')
         res.render('passGenerator/home')
     })
 
+    router.post('/gerarsenha', (req, res)=>{
+    
 
+       let tipoSenha = req.body.flexRadioDefault
+       let tamSenha = req.body.tamanho
+       let password = ''
+       var chars = ''
+
+       if(tipoSenha === 'num'){
+           chars = '0123456789'
+       } else if(tipoSenha === 'letras'){
+           chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ'
+       } else {
+           chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ'
+       }
+
+       
+
+       for (var i = 0; i < tamSenha; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber + 1);
+        }
+
+        res.render('passGenerator/home', {senha: password, tamanho: tamSenha})
+
+    })
 
 
 
